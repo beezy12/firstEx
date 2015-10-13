2de76fd8a3e7fe1e1ad8a20a917ac38c254ad205
Brian Stumbaugh <brian.stumbuagh@gmail.com>
2015-10-13 09:33:58 -0500

    initial commit

---
+++ reindeer/reindeer.js
@@ -0,0 +1,42 @@
+/*
+  YOUR ASSIGNMENT
+  ----------------------------------------
+  Loop through all the reindeer in the array, and add the 
+  name of the reindeer to the single HTML <div> element provided.
+  The name of the reindeer should be prepended with the corresponding
+  color from the other array.
+  
+  For example:
+    Blue Dasher
+    Red Dancer
+    etc..
+*/
+
+
+
+var colors = ["Blue", "Red", "Orange", "Purple", "Hazel", "Aquamarine", "Periwinkle", "Azure", "Fuchsia", "Chocolate", "Amber", "Amaranth"];
+var reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"];
+
+var hohohoElement = document.getElementById("reindeer");
+
+var output = "";   //output just creates a string
+
+for (i = 0; i < reindeer.length; i++) {    //chose reindeer.length bc it was a shorter array
+  var currentReindeer = reindeer[i];
+  var currentColor = colors[i];
+
+  output = output + "<div>";   //javascript will read anything not in quotes as a variable
+  output = output + currentColor + " " + currentReindeer;
+  output = output + "</div>"
+}
+
+console.log(output);
+hohohoElement.innerHTML = output;
+
+/* var myStringArray = ["Hello","World"];
+var arrayLength = myStringArray.length;
+for (var i = 0; i < arrayLength; i++) {
+    alert(myStringArray[i]);
+    //Do something     this was taken from online at stackoverflow
+}  */
+
